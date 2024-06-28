bfcd37400f3a1d443ad85061fa8086f2b2e12b09
Kevin Friend <kevin@proofpilot.com>
2024-06-28 09:18:52 -0400

    Add support for `_tests.go` files

--- dist/index.js
+++ dist/index.js
@@ -1875,72 +1875,75 @@ const core = __nccwpck_require__(186);
 const lineReader = __nccwpck_require__(752);
 const fs = __nccwpck_require__(147);
 
-
 try {
-	const regex = /(\s*[\w\d]+_test.go:\d+:)(.*?)(Test:\s+Test[\w\d]*?\S+)/gu; // Extracts only the failure from the logs (including whitespace)
+  const regex = /(\s*[\w\d]+_test.go:\d+:)(.*?)(Test:\s+Test[\w\d]*?\S+)/gu; // Extracts only the failure from the logs (including whitespace)
 
-	const testResultsPath = core.getInput('test-results');
-	const customPackageName = core.getInput('package-name');
-	const workingDirectory = core.getInput('working-directory');
+  const testResultsPath = core.getInput('test-results');
+  const customPackageName = core.getInput('package-name');
+  const workingDirectory = core.getInput('working-directory');
 
-	if (!fs.existsSync(testResultsPath)) {
-		core.warning(
-			`No file was found with the provided path: ${testResultsPath}.`
-		)
-		return
-	}
+  if (!fs.existsSync(testResultsPath)) {
+    core.warning(
+      `No file was found with the provided path: ${testResultsPath}.`
+    );
+    return;
+  }
 
-	let obj = {};
-	let lr = new lineReader(testResultsPath);
-	lr.on('line', function (line) {
-		const currentLine = JSON.parse(line);
-		const testName = currentLine.Test;
-		if (typeof testName === "undefined") {
-			return;
-		}
+  let obj = {};
+  let lr = new lineReader(testResultsPath);
+  lr.on('line', function (line) {
+    const currentLine = JSON.parse(line);
+    const testName = currentLine.Test;
+    if (typeof testName === 'undefined') {
+      return;
+    }
 
-		let output = currentLine.Output;
-		if (typeof output === "undefined") {
-			return;
-		}
-		output = output.replace("\n", "%0A").replace("\r", "%0D")
-		// Strip github.com/owner/repo package from the path by default
-		let packageName = currentLine.Package.split("/").slice(3).join("/");
-		// If custom package is provided, strip custom package name from the path
-		if (customPackageName !== "") {
-			if (!currentLine.Package.startsWith(customPackageName)) {
-				core.warning(
-					`Expected ${currentLine.Package} to start with ${customPackageName} since "package-name" was provided.`
-				)
-			} else {
-				packageName = currentLine.Package.replace(customPackageName + "/", "")
-			}
-		}
-		if (workingDirectory !== "") {
-			packageName = workingDirectory + "/" + packageName
-		}
-		let newEntry = packageName + "/" + testName;
-		if (!obj.hasOwnProperty(newEntry)) {
-			obj[newEntry] = output;
-		} else {
-			obj[newEntry] += output;
-		}
-	});
-	lr.on('end', function () {
-		for (const [key, value] of Object.entries(obj)) {
-			if (value.includes("FAIL") && value.includes("_test.go")) {
-				var result;
-				while ((result = regex.exec(value)) !== null) {
-					const parts = result[0].split(":");
-					const file = key.split("/").slice(0, -2).join("/") + "/" + parts[0].trimStart();
-					const lineNumber = parts[1];
-					core.info(`::error file=${file},line=${lineNumber}::${result[0]}`);
-				}
-			}
-		}
-	});
+    let output = currentLine.Output;
+    if (typeof output === 'undefined') {
+      return;
+    }
+    output = output.replace('\n', '%0A').replace('\r', '%0D');
+    // Strip github.com/owner/repo package from the path by default
+    let packageName = currentLine.Package.split('/').slice(3).join('/');
+    // If custom package is provided, strip custom package name from the path
+    if (customPackageName !== '') {
+      if (!currentLine.Package.startsWith(customPackageName)) {
+        core.warning(
+          `Expected ${currentLine.Package} to start with ${customPackageName} since "package-name" was provided.`
+        );
+      } else {
+        packageName = currentLine.Package.replace(customPackageName + '/', '');
+      }
+    }
+    if (workingDirectory !== '') {
+      packageName = workingDirectory + '/' + packageName;
+    }
+    let newEntry = packageName + '/' + testName;
+    if (!obj.hasOwnProperty(newEntry)) {
+      obj[newEntry] = output;
+    } else {
+      obj[newEntry] += output;
+    }
+  });
+  lr.on('end', function () {
+    for (const [key, value] of Object.entries(obj)) {
+      if (
+        value.includes('FAIL') &&
+        (value.includes('_test.go') || value.includes('_tests.go'))
+      ) {
+        var result;
+        while ((result = regex.exec(value)) !== null) {
+          const parts = result[0].split(':');
+          const file =
+            key.split('/').slice(0, -2).join('/') + '/' + parts[0].trimStart();
+          const lineNumber = parts[1];
+          core.info(`::error file=${file},line=${lineNumber}::${result[0]}`);
+        }
+      }
+    }
+  });
 } catch (error) {
-	core.setFailed(error.message);
+  core.setFailed(error.message);
 }
 
 })();
